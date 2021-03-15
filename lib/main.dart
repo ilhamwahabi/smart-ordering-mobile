16dd0cae5abb694f588b30fd1ec00958675af3d3
iwgx <wahabiputra@gmail.com>
2021-03-15 14:55:34 +0700

    Able to connect mqtt

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Smart Ordering',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Smart Ordering'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,68 +29,98 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  MqttServerClient client = MqttServerClient.withPort(
+    '202.148.1.57',
+    'flutter_client',
+    1883,
+  );
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  Future<MqttServerClient> connect() async {
+    client.logging(on: true);
+    client.onConnected = () {
+      print('Connected');
+    };
+    client.onDisconnected = () {
+      print('Disconnected');
+    };
+    client.onUnsubscribed = (String topic) {
+      print('Unsubscribed topic: $topic');
+    };
+    client.onSubscribed = (String topic) {
+      print('Subscribed topic: $topic');
+    };
+    client.onSubscribeFail = (String topic) {
+      print('Failed to subscribe $topic');
+    };
+    client.pongCallback = () {
+      print('Ping response client callback invoked');
+    };
+
+    final connMessage = MqttConnectMessage().authenticateAs(
+      'app-smartorderingsystem',
+      'G4zwVj1B1qmTDR2V0oY7y2YVqUUe6o',
+    );
+    // .keepAliveFor(60)
+    // .withWillTopic('willtopic')
+    // .withWillMessage('Will message')
+    // .startClean()
+    // .withWillQos(MqttQos.atLeastOnce);
+    client.connectionMessage = connMessage;
+    try {
+      await client.connect();
+    } catch (e) {
+      print('Exception: $e');
+      client.disconnect();
+    }
+
+    client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final MqttPublishMessage message = c[0].payload;
+      final payload =
+          MqttPublishPayload.bytesToStringAsString(message.payload.message);
+
+      print('Received message:$payload from topic: ${c[0].topic}>');
     });
+
+    return client;
+  }
+
+  void subscribe() {
+    client.subscribe("topic/test", MqttQos.atLeastOnce);
+  }
+
+  void publish() {
+    const pubTopic = 'topic/test';
+    final builder = MqttClientPayloadBuilder();
+    builder.addString('Hello MQTT');
+    client.publishMessage(pubTopic, MqttQos.atLeastOnce, builder.payload);
+  }
+
+  void unsubscribe() {
+    client.unsubscribe('topic/test');
+  }
+
+  void disconnect() {
+    client.disconnect();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                connect();
+              },
+              child: Text("Connect"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
